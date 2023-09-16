c9689f6939ee3cdcb36192f88fed9eee62dd2047
tyronemurg <tyronemurg@gmail.com>
2023-09-16 13:30:00 +0200

    Hungy Hippo Animation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SliderComponent } from './pages/slider/slider.component';
+import { HippoComponent } from './pages/hippo/hippo.component';
 
 
 const routes: Routes = [
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path: 'slider',
     component: SliderComponent
+  },
+  {
+    path: 'hippo',
+    component: HippoComponent
   },
 
 ];
